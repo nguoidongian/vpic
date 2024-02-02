4314d4760c421972a535f692627b181a8281e709
nguoidongian <47925819+nguoidongian@users.noreply.github.com>
2024-02-02 20:50:24 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -15,7 +15,7 @@ uploaded_file_tonlovitri = st.file_uploader("Chọn file: ", type=["xlsx", "xls"
 
 if uploaded_file_tonlovitri is not None:
         # Đọc dữ liệu từ file Excel
-    df_tonlovitri = pd.read_excel(uploaded_file_tonlovitri,
+        df_tonlovitri = pd.read_excel(uploaded_file_tonlovitri, engine="xlrd", sheet_name="Data", usecols='A:J', header=10)
                             engine="openpyxl",
                             sheet_name="Data",
                             usecols='A:J',
