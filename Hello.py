eae48233e8c7d3e97af65e000415b5e36712d356
nguoidongian <47925819+nguoidongian@users.noreply.github.com>
2024-02-22 14:50:18 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -6,13 +6,20 @@ import plotly.express as px
 from PIL import Image
 from streamlit_option_menu import option_menu
 from datetime import datetime
-
+import numpy as np
 
 st.set_page_config(page_title='KHO VPIC1',layout="wide")
 # Báo cáo số vị trí các mã  
+column1, column2, column3 = st.columns(3)
+with column1:
+    st.subheader("Chọn file Excel Tồn kho ERP ( Đối chiếu tồn kho ERP - WMS )")
+    uploaded_file_tonkhoerp = st.file_uploader("Chọn file: ", type=["xlsx", "xls"], key="tonkhoerp")
+with column2:
+    st.subheader("Chọn file Excel Tồn theo kho WMS ( Đối chiếu tồn kho ERP - WMS )")
+    uploaded_file_tonkhowms = st.file_uploader("Chọn file: ", type=["xlsx", "xls"], key="tonkhowms")
+with column3:
+    st.subheader("Lệch tồn các kho")
 
-st.subheader("Chọn file Excel Tồn kho ERP ( Đối chiếu tồn kho ERP - WMS )")
-uploaded_file_tonkhoerp = st.file_uploader("Chọn file: ", type=["xlsx", "xls"], key="tonkhoerp")
 
 
 if uploaded_file_tonkhoerp is not None:
@@ -24,8 +31,7 @@ if uploaded_file_tonkhoerp is not None:
     df_tonkhoerp_selected_columns = df_tonkhoerp[selected_tonkhoerp_columns]
     df_tonkhoerp_selected_columns['Kho'] = df_tonkhoerp_selected_columns['Kho'].str.strip()
 
-st.subheader("Chọn file Excel Tồn theo kho WMS ( Đối chiếu tồn kho ERP - WMS )")
-uploaded_file_tonkhowms = st.file_uploader("Chọn file: ", type=["xlsx", "xls"], key="tonkhowms")
+
 
 if uploaded_file_tonkhowms is not None:
         # Đọc dữ liệu từ file Excel
@@ -60,7 +66,7 @@ if uploaded_file_tonkhowms is not None:
                         & (merged_df['Kho'] != 'TS:')]
 
 
-    df_Khodachay = pd.read_excel(io="data/KHO CHẠY WMS.xlsx",
+    df_Khodachay = pd.read_excel(io="data\khochaywms.xlsx",
                              engine="openpyxl",
                              sheet_name="Data",
                              usecols='A:D',
@@ -75,34 +81,37 @@ if uploaded_file_tonkhowms is not None:
 
     merged_df_final = merged_df_final.dropna(subset=['Mã kho', 'Mã SP'])
     total_somalecherp = int(merged_df_final["Mã SP"].count())
-
-    st.subheader("Đối chiếu tồn kho chiều ERP -> WMS"+ " Lệch "+ " " + (str(total_somalecherp)) + " " + "mã")
-    st.dataframe(merged_df_final)
+    column3, column4 = st.columns(2)
+    with column3:
+        st.subheader("Đối chiếu tồn kho chiều ERP -> WMS"+ " Lệch "+ " " + (str(total_somalecherp)) + " " + "mã")
+        st.dataframe(merged_df_final)
 
 # Đối chiếu tồn kho WMS - ERP
 
-    merged_WMS_df = pd.merge(df_tonkhowms_selected_columns,df_tonkhoerp_selected_columns,
+        merged_WMS_df = pd.merge(df_tonkhowms_selected_columns,df_tonkhoerp_selected_columns,
                      left_on=['Mã vật tư ERP', 'Mã Kho'], right_on=['Mã SP', 'Kho'],
                      how='outer', suffixes=('_tonkhowms', '_tonkhoerp'))
-    merged_WMS_df = merged_WMS_df.fillna(0)
-    merged_WMS_df['Chênh lệch'] = merged_WMS_df['SL tồn kho'] - merged_WMS_df['Giá trị']
-    filtered_WMS_df = merged_WMS_df[(merged_WMS_df['Chênh lệch'] != 0)
+        merged_WMS_df = merged_WMS_df.fillna(0)
+        merged_WMS_df['Chênh lệch'] = merged_WMS_df['SL tồn kho'] - merged_WMS_df['Giá trị']
+        filtered_WMS_df = merged_WMS_df[(merged_WMS_df['Chênh lệch'] != 0)
                         & (merged_WMS_df['Kho'] != 'Tất:')
                         & (merged_WMS_df['Kho'] != 'TS:')]
 
-    merged_WMS_df_final = pd.merge(filtered_WMS_df, df_Khodachay,
+        merged_WMS_df_final = pd.merge(filtered_WMS_df, df_Khodachay,
                      left_on=['Mã Kho'], right_on=['Mã kho'],
                      how='outer', suffixes=('_tonkhowms', '_Khodachay'))
 
-    merged_WMS_df_final = merged_WMS_df_final.dropna(subset=['Mã kho', 'Mã SP'])
-    total_somalechwms = int(merged_WMS_df_final["Mã vật tư ERP"].count())
+        merged_WMS_df_final = merged_WMS_df_final.dropna(subset=['Mã kho', 'Mã SP'])
+        total_somalechwms = int(merged_WMS_df_final["Mã vật tư ERP"].count())
 
-    st.subheader("Đối chiếu tồn kho chiều WMS -> ERP"+ " Lệch "+ " " + (str(total_somalechwms)) + " " + "mã")
+    with column4:
+        st.subheader("Đối chiếu tồn kho chiều WMS -> ERP"+ " Lệch "+ " " + (str(total_somalechwms)) + " " + "mã")
+        st.dataframe(merged_WMS_df_final)
+column5, column6 = st.columns(2)
+with column5:
+    st.subheader("Chọn file Excel WMS tồn lô vị trí ( Các mã có 2 vị trí trở lên )")
+    uploaded_file_tonlovitri = st.file_uploader("Chọn file: ", type=["xlsx", "xls"], key="tonlovitri")
 
-    st.dataframe(merged_WMS_df_final)
-    
-st.subheader("Chọn file Excel WMS tồn lô vị trí ( Các mã có 2 vị trí trở lên )")
-uploaded_file_tonlovitri = st.file_uploader("Chọn file: ", type=["xlsx", "xls"], key="tonlovitri")
 
 if uploaded_file_tonlovitri is not None:
         # Đọc dữ liệu từ file Excel
@@ -123,9 +132,10 @@ if uploaded_file_tonlovitri is not None:
     selected_columns_manhieuvitri = ['Mã vật tư ERP', 'Tên vật tư','Mã kho ERP', 'Quy cách quản lý','ĐVT','Mã vị trí','Tồn vị trí','Số vị trí']
     df_manhieuvitri_selected_columns = df_maNhieuViTri[selected_columns_manhieuvitri]
 
-    st.subheader("Những mã có 2 vị trí")
-    st.dataframe(df_manhieuvitri_selected_columns)
-
+    
+    with column6:
+        st.subheader("Những mã có 2 vị trí trở lên")
+        st.dataframe(df_manhieuvitri_selected_columns) 
 st.subheader("Chọn file Excel Tồn kho tem thùng theo vị trí WMS ( Các mã lệch tem thùng )")
 uploaded_file_tonkhotemthung = st.file_uploader("Chọn file: ", type=["xlsx", "xls"], key="tonkhotemthung")
 if uploaded_file_tonkhotemthung is not None:
@@ -184,17 +194,24 @@ if uploaded_file_fifo is not None:
     df_fifo_merged['Kết quả'] = (df_fifo_merged['Mã vật tư ERP'] == df_fifo_merged['Mã vật tư ERP'].shift(-1)) & \
                             (df_fifo_merged['Tồn cuối'] != 0) & \
                             (df_fifo_merged['Số lượng xuất'].shift(-1) != 0)
-    df_fifo_merged['Kết quả'] = df_fifo_merged['Kết quả'].map({True: 'Sai', False: 'Đúng'})
+    df_fifo_merged['Kết quả'] = df_fifo_merged['Kết quả'].map({True: 'Sai', False: '0'})
   
     df_filtered = df_fifo_merged.loc[df_fifo_merged['Kết quả'] == 'Sai']
     df_filtered = df_filtered.sort_values(by=['Mã vật tư ERP', 'Mã lô'], ascending=True)
     df_fifo_merged_final = pd.merge(df_fifo, df_filtered, on='Mã vật tư ERP', how='left')
-    df_fifo_selected_columns = ['STT_x', 'Mã lô_x', 'Mã vật tư ERP','Tên vật tư_x','Quy cách quản lý_x','Tồn đầu_x','Số lượng nhập_x','Số lượng xuất_x', 'Tồn cuối_x', 'Số lượng mã lô','Kết quả']
-    df_fifo_selected_columns_final = df_fifo_merged_final[df_fifo_selected_columns]
-    df_fifo_cleaned = df_fifo_selected_columns_final.dropna(subset=['Kết quả'])
+    df_fifo_merged_final_1 = pd.merge(df_fifo_merged, df_filtered, on='Mã vật tư ERP', how='left')
+
+    df_fifo_selected_columns = ['STT_x', 'Mã lô_x', 'Mã vật tư ERP','Tên vật tư_x','Quy cách quản lý_x','Tồn đầu_x','Số lượng nhập_x','Số lượng xuất_x', 'Tồn cuối_x', 'Số lượng mã lô_x','Kết quả_x','Kết quả_y']
+    df_fifo_selected_columns_final = df_fifo_merged_final_1[df_fifo_selected_columns]
+    df_fifo_cleaned = df_fifo_selected_columns_final.dropna(subset=['Kết quả_y'])
     total_mavattufifo= int(df_fifo_cleaned["Mã vật tư ERP"].count())
     count_dem_ma_sai = df_fifo_cleaned["Mã vật tư ERP"].value_counts().count()
+    
+    def highlight_sai(s):
+        return ['background-color: yellow' if val == 'Sai' else '' for val in s]
 
+    df_fifo_cleaned.style.apply(highlight_sai, subset=['Kết quả_x'])
+   
 # Hiển thị DataFrame
     
 
@@ -219,11 +236,11 @@ if uploaded_file_fifo is not None:
 
 
 
-    column1, column2 = st.columns(2)
-    with column1:
+    column11, column12 = st.columns(2)
+    with column11:
         st.subheader("FiFo sai đúng")
-        st.dataframe(df_fifo_cleaned)
-    with column2:
+        st.dataframe(df_fifo_cleaned.style.apply(highlight_sai, subset=['Kết quả_x']))
+    with column12:
         st.subheader("Số mã sai " + ": " + str(count_dem_ma_sai) + "/ " + "Số lượng mã: " + " " + str(count_dem_ma_fifo))
         st.plotly_chart(fig_pie)
 
