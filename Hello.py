480359fbd7f203b16b124fa3eeaac42235764d7e
nguoidongian <47925819+nguoidongian@users.noreply.github.com>
2024-04-03 10:09:29 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -2,35 +2,39 @@ import streamlit as st
 import csv
 from datetime import datetime
 
-def save_to_csv(data, filename=r'\data.csv'):
+def save_to_csv(data, filename=r'D:\New folder\data.csv'):
     with open(filename, 'a', newline='', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(data)
+        writer.writerows(data)
 
 def main():
     st.title("Ứng dụng quét và lưu vào CSV")
 
-    # Lấy ngày và giờ hiện tại
     current_datetime = datetime.now()
 
-    # Hiển thị ngày và giờ
     st.write(f"Ngày: {current_datetime.date()}")
     st.write(f"Giờ: {current_datetime.time()}")
 
-    # Tạo ô input cho người dùng nhập tên, email và số điện thoại
-    name = st.text_area("quét tên:", height=30)
+    with st.form(key='my_form'):
+        name = st.text_area("quét tên:", height=30)
+        order_code = st.text_input("Mã đơn:")
+        
+        option = st.selectbox("Chọn loại:", ["Nhập", "Xuất"])
 
-    # Nếu người dùng nhấn nút "Lưu", lưu dữ liệu vào file CSV
-    if st.button("Lưu"):
-        # Format ngày và giờ thành chuỗi
-        datetime_str = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
-        # Tách dữ liệu thành từng dòng
-        lines = name.split('\n')
-        # Lưu mỗi dòng dữ liệu với ngày giờ hiện tại
-        for line in lines:
-            data = [datetime_str, line.strip()]
+        submitted = st.form_submit_button("Lưu")
+
+        if submitted:
+            datetime_str = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+            lines_name = name.split('\n')
+            data = [[datetime_str, line.strip(), order_code.strip(), option] for line in lines_name]
             save_to_csv(data)
-        st.success("Dữ liệu đã được lưu thành công vào file CSV.")
+            st.success("Dữ liệu đã được lưu thành công vào file CSV.")
+
+    if st.button("Xuất"):
+        with open(r'D:\New folder\data.csv', 'r', newline='', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                st.write(row)
 
 if __name__ == "__main__":
     main()
